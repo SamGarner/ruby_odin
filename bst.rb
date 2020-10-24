134f5e7661d015714ba1e050353196036a2cdc61
SamGarner <samuel.w.garner@gmail.com>
2020-10-23 20:46:13 -0500

    troubleshooting example typed out and pretty print added

--- bst.rb
+++ bst.rb
@@ -19,30 +19,82 @@ end
 
 # tree class
 class Tree
-  attr_reader :sorted_array
-  attr_accessor :root
+  attr_reader :sorted_array, :mid, :tree_array
+  attr_accessor :root, :node
 
   def initialize(array)
-    @tree_array = array
-    @root = Node.new(nil)
+    tree_array = array.uniq.sort
+    @root = build_tree(tree_array)
   end
 
-  def build_tree(array, first_index = 0, last_index = array.length - 1)
-    # NEED STOPPING POINT FOR RECURSION
+  def build_tree(tree_array, first_index = 0, last_index = tree_array.length - 1)
     return nil if first_index > last_index
-    
-    @sorted_array = array.uniq.sort # is this getting reassigned ? 
-    len = sorted_array.length
-    mid = (len / 2).floor
-    # first_index = 0
-    # last_index = len - 1
-    root = Node.new(sorted_array[mid])
-    # self.root.value = sorted_array[mid]
-    root.left_child = build_tree(sorted_array[0..mid-1], 0, mid-1)
-    root.right_child = build_tree(sorted_array[mid+1..len-1], mid, len - 1)
+
+    mid = (first_index + last_index)/2.floor
+    # @last_index = last_index
+    root = Node.new(tree_array[mid])
+
+    root.left_child = build_tree(tree_array, 0, mid - 1)
+    root.right_child = build_tree(tree_array, mid + 1, last_index)
+
     return root
   end
+
+troubleshooting: Tree.new([3, 1, 2])
+
+build_tree([1, 2, 3], 0, 2)
+mid = 1
+root = Node (value = 2)
+root.left_child = build_tree([1, 2, 3], 0, 0)
+  mid = 0
+  root = Node (value = 1)
+  root.left_child = build_tree([1, 2, 3], 0, -1)
+    nil
+  root.right_child = build_tree([1, 2, 3], 1, 0) ## is last index pulling in right?
+    nil
+root.right_child = build_tree([1, 2, 3], 2, 2)
+  mid = 2
+  root = Node (value = 3)
+  root.left_child = build_tree([1, 2, 3], 0, 1)
+    mid = 0
+    root = 1 # INCORRECT!
+    root.left_child = build_tree([1, 2, 3], 0, -1)
+      nil
+    root.right_child = build_tree([1, 2, 3], 1, 2)
+      mid = 1
+      root = 2 # INCORRECT
+      root.left_child = build_tree([1, 2, 3], 0, 0)
+      root.right_child = build_tree([1, 2, 3], 2, 2)
+  root.right_child = build_tree([1, 2, 3], 3, 2)
+    nil
+
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
+  end
 end
+
+p test_tree = Tree.new([3, 1, 2])
+test_tree.pretty_print
+
+#   def build_tree(array, first_index = 0, last_index = array.length - 1)
+#     # NEED STOPPING POINT FOR RECURSION
+#     return nil if first_index > last_index
+    
+#     @sorted_array = array.uniq.sort # is this getting reassigned ? 
+#     len = sorted_array.length
+#     mid = (len / 2).floor
+#     # first_index = 0
+#     # last_index = len - 1
+#     root = Node.new(sorted_array[mid])
+#     # self.root.value = sorted_array[mid]
+#     root.left_child = build_tree(sorted_array[0..mid-1], 0, mid-1)
+#     root.right_child = build_tree(sorted_array[mid+1..len-1], mid, len - 1)
+#     return root
+#   end
+# end
 
 # /example walkthrough
 #   build_tree([3,1,2])
